cf3671371cbc25447fb73fbb4bd45994f790d64c
joelita-lima <joelitalima77@gmail.com>
2024-10-22 19:36:53 -0300

    Update covid-cases.py

--- covid-cases.py
+++ covid-cases.py
@@ -1,2 +1,13 @@
+# Pacotes nativos do Python
 import numpy as np
 import pandas as pd
+import math
+from typing import Iterator
+from datetime import datetime, timedelta
+
+#Extração
+#Esse código utiliza a biblioteca pandas para ler um arquivo CSV diretamente de um URL. A variável cases armazena os dados do CSV sobre COVID-19, exemplo para 01/12/2021.
+
+cases = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/01-12-2021.csv', sep=',')
+
+cases.head()
