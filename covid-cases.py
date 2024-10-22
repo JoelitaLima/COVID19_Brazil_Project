66b6584b1db94c731188d31480f037b14709426f
joelita-lima <joelitalima77@gmail.com>
2024-10-22 19:46:11 -0300

    Update covid-cases.py

--- covid-cases.py
+++ covid-cases.py
@@ -10,4 +10,140 @@ from datetime import datetime, timedelta
 
 cases = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/01-12-2021.csv', sep=',')
 
+#Portanto, precisaremos iterar dentro de um intervalo de tempo definido para extraí-lo.
+def date_range(start_date: datetime, end_date: datetime) -> Iterator[datetime]:
+  date_range_days: int = (end_date - start_date).days
+  for lag in range(date_range_days):
+    yield start_date + timedelta(lag)
+
+start_date = datetime(2021,  1,  1)
+end_date   = datetime(2021, 12, 31)
+
+#Este código itera sobre um intervalo de datas e, para cada data, baixa um arquivo CSV contendo dados de COVID-19. Em seguida, ele filtra as colunas de interesse e mantém apenas as linhas referentes ao Brasil.
+cases = None
+cases_is_empty = True
+
+for date in date_range(start_date=start_date, end_date=end_date):
+
+  date_str = date.strftime('%m-%d-%Y')
+  data_source_url = f'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{date_str}.csv'
+
+  case = pd.read_csv(data_source_url, sep=',')
+
+  case = case.drop(['FIPS', 'Admin2', 'Last_Update', 'Lat', 'Long_', 'Recovered', 'Active', 'Combined_Key', 'Case_Fatality_Ratio'], axis=1)
+  case = case.query('Country_Region == "Brazil"').reset_index(drop=True)
+  case['Date'] = pd.to_datetime(date.strftime('%Y-%m-%d'))
+  if cases_is_empty:
+    cases = case
+    cases_is_empty = False
+  else:
+    cases = pd.concat([cases, case], axis= 0, ignore_index=True)
+
+cases.query('Province_State == "Sao Paulo"').head()
+
+#Transformação
+#Vamos manipular os dados para o dashboard. O foco é em garantir uma boa granularidade e qualidade da base de dados.
+
 cases.head()
+
+#Essa combinação de comandos ajuda a obter uma visão geral rápida e detalhada do DataFrame.
+
+cases.shape
+
+cases.info()
+
+#Começamos com o nome das colunas.
+cases = cases.rename(
+  columns={
+    'Province_State': 'state',
+    'Country_Region': 'country'
+  }
+)
+
+for col in cases.columns:
+  cases = cases.rename(columns={col: col.lower()})
+
+#Ajustamos o nome dos estados.
+states_map = {
+    'Amapa': 'Amapá',
+    'Ceara': 'Ceará',
+    'Espirito Santo': 'Espírito Santo',
+    'Goias': 'Goiás',
+    'Para': 'Pará',
+    'Paraiba': 'Paraíba',
+    'Parana': 'Paraná',
+    'Piaui': 'Piauí',
+    'Rondonia': 'Rondônia',
+    'Sao Paulo': 'São Paulo'
+}
+
+cases['state'] = cases['state'].apply(lambda state: states_map.get(state) if state in states_map.keys() else state)
+
+#Vamos então computar novas colunas para enriquecer a base de dados.
+
+#Chaves temporais:
+cases['month'] = cases['date'].apply(lambda date: date.strftime('%Y-%m'))
+cases['year']  = cases['date'].apply(lambda date: date.strftime('%Y'))
+
+#População estimada do estado:
+cases['population'] = round(100000 * (cases['confirmed'] / cases['incident_rate']))
+cases = cases.drop('incident_rate', axis=1)
+
+#Este código processa dados diários de COVID-19 por estado, calculando tendências de casos e óbitos com médias móveis.Filtrando, transformando e combinando esses dados em um único DataFrame para análise detalhada.
+cases_ = None
+cases_is_empty = True
+
+def get_trend(rate: float) -> str:
+
+  if np.isnan(rate):
+    return np.NaN
+
+  if rate < 0.75:    # Se a divisão for menor que 0.75 (correção 0.85)
+    status = 'downward'
+  elif rate > 1.15:
+    status = 'upward'
+  else:
+    status = 'stable'
+
+  return status
+
+
+for state in cases['state'].drop_duplicates():
+
+  cases_per_state = cases.query(f'state == "{state}"').reset_index(drop=True)
+  cases_per_state = cases_per_state.sort_values(by=['date'])
+
+  cases_per_state['confirmed_1d'] = cases_per_state['confirmed'].diff(periods=1)
+  cases_per_state['confirmed_moving_avg_7d'] = np.ceil(cases_per_state['confirmed_1d'].rolling(window=7).mean())
+  cases_per_state['confirmed_moving_avg_7d_rate_14d'] = cases_per_state['confirmed_moving_avg_7d']/cases_per_state['confirmed_moving_avg_7d'].shift(periods=14)
+  cases_per_state['confirmed_trend'] = cases_per_state['confirmed_moving_avg_7d_rate_14d'].apply(get_trend)
+
+  cases_per_state['deaths_1d'] = cases_per_state['deaths'].diff(periods=1)
+  cases_per_state['deaths_moving_avg_7d'] = np.ceil(cases_per_state['deaths_1d'].rolling(window=7).mean())
+  cases_per_state['deaths_moving_avg_7d_rate_14d'] = cases_per_state['deaths_moving_avg_7d']/cases_per_state['deaths_moving_avg_7d'].shift(periods=14)
+  cases_per_state['deaths_trend'] = cases_per_state['deaths_moving_avg_7d_rate_14d'].apply(get_trend)
+
+  if cases_is_empty:
+    cases_ = cases_per_state
+    cases_is_empty = False
+  else:
+    cases_ = pd.concat([cases_, cases_per_state],axis=0, ignore_index=True)
+
+cases = cases_
+cases_ = None
+
+#Garantir o tipo do dado é fundamental para consistência da base de dados. Vamos fazer o type casting das colunas.
+cases['population'] = cases['population'].astype('Int64')
+cases['confirmed_1d'] = cases['confirmed_1d'].astype('Int64')
+cases['confirmed_moving_avg_7d'] = cases['confirmed_moving_avg_7d'].astype('Int64')
+cases['deaths_1d'] = cases['deaths_1d'].astype('Int64')
+cases['deaths_moving_avg_7d'] = cases['deaths_moving_avg_7d'].astype('Int64')
+
+#Por fim, vamos reorganizar as colunas e conferir o resultado final.
+cases = cases[['date', 'country', 'state', 'population', 'confirmed', 'confirmed_1d', 'confirmed_moving_avg_7d', 'confirmed_moving_avg_7d_rate_14d', 'confirmed_trend', 'deaths', 'deaths_1d', 'deaths_moving_avg_7d', 'deaths_moving_avg_7d_rate_14d', 'deaths_trend', 'month', 'year']]
+cases.head()
+
+#Carregamento
+Com os dados manipulados, vamos persisti-lo em disco, fazer o seu download e carrega-lo no Google Data Studio.
+
+cases.to_csv('./covid-cases.csv', sep=',', index=False)
